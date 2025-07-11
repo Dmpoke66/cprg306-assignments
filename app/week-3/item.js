3053753cd9bac4f99ed0ca57399ff9b64334d49d
Dmpoke66 <Daniel.Maliczewski@edu.sait.ca>
2025-07-10 22:06:39 -0600

    Delete app/week-3 directory

--- app/week-3/item.js
+++
@@ -1,9 +0,0 @@
-export default function Item({ name, quantity, category }) {
-  return (
-    <li className="border rounded-lg p-4 mb-2 shadow-sm bg-white">
-      <p className="text-lg font-semibold">{name}</p>
-      <p className="text-sm">Quantity: {quantity}</p>
-      <p className="text-sm">Category: {category}</p>
-    </li>
-  );
-}
