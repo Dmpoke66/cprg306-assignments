26353ca620a7201a72f906824f35913197e0fab5
Dmpoke66 <Daniel.Maliczewski@edu.sait.ca>
2025-07-10 22:07:47 -0600

    Add files via upload

---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <p>Daniel Maliczewski</p>
+      <p>
+        <Link href="https://github.com/Dmpoke66/cprg306-assignments" target="_blank">
+          https://github.com
+        </Link>
+      </p>
+    </div>
+  );
+}
