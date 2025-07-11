7b0663858d66f5de81bb81ba93d7216e5c839829
Dmpoke66 <Daniel.Maliczewski@edu.sait.ca>
2025-07-10 22:06:46 -0600

    Delete app/week-4 directory

--- app/week-4/page.js
+++
@@ -1,9 +0,0 @@
-import NewItem from './new-item';
-
-export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <NewItem />
-    </div>
-  );
-}
